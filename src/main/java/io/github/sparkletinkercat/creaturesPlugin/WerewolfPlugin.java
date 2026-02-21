0fe90afa2f63bca8af34773dc20cc334628ee5d9
Carinn Gaspar <gasca681@student.otago.ac.nz>
2026-02-21 20:27:40 +1300

    Added a way to see the tags when you join

--- src/main/java/io/github/sparkletinkercat/creaturesPlugin/WerewolfPlugin.java
+++ src/main/java/io/github/sparkletinkercat/creaturesPlugin/WerewolfPlugin.java
@@ -2,19 +2,33 @@ package io.github.sparkletinkercat.creaturesPlugin;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Set;
+
 public class WerewolfPlugin extends JavaPlugin implements Listener {
+  
+  @Override
+  public void onLoad() {}
+  
   @Override
   public void onEnable() {
     Bukkit.getPluginManager().registerEvents(this, this);
   }
 
+  @Override
+  public void onDisable() {}
+
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent event) {
-    event.getPlayer().sendMessage(Component.text("Hello there, " + event.getPlayer().getName() + "!"));
+    Player player = event.getPlayer();
+    Set<String> tags = player.getScoreboardTags();
+    String tagsString = String.join(", ", tags);
+    
+    player.sendMessage("Your tags:" + tagsString);
   }
 }
